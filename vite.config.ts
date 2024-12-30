e8bfff18afb73091f0421ad9b01ae8133468c980
Nordic-International-University <info@nordicuniversity.org>
2024-12-30 17:12:10 +0500

    vite config production uchun o'zgartirildi

--- vite.config.ts
+++ vite.config.ts
@@ -4,30 +4,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export default defineConfig(({ mode }) => {
-  const isProduction = mode === "production";
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: process.env.VITE_BACKEND_URL_MAIN || "http://localhost:4040", // Zaxira URL qo'shildi
-          changeOrigin: true,
-          secure: false,
-        },
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.VITE_BACKEND_URL_MAIN,
+        changeOrigin: true,
+        secure: false,
       },
     },
-    build: {
-      outDir: "dist", // Railway statik fayllarni `dist`dan oladi
-      sourcemap: isProduction, // Ishlab chiqarishda sourcemap qo'shiladi
-    },
-    define: {
-      "process.env": {
-        VITE_BACKEND_URL_MAIN: JSON.stringify(
-          process.env.VITE_BACKEND_URL_MAIN,
-        ),
-      },
-    },
-  };
+  },
 });
